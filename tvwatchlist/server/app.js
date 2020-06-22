91c3076660527038c81497fb22deed36606d6755
kadjiah <kadijahwilson@pursuit.org>
2020-06-21 19:27:56 -0500

    added routes to app.use in app.js file

--- tvwatchlist/server/app.js
+++ tvwatchlist/server/app.js
@@ -2,19 +2,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/Users');
+var commentsRouter = require('./routes/Comments');
+var genresRouter = require('./routes/Genres');
+var showRouter = require('./routes/Shows');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
+app.use('/genres', genresRouter);
+app.use('/shows', showRouter);
+
 
 module.exports = app;
