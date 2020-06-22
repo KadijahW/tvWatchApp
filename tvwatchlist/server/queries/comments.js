278597c088fca37d2845bc82b883cd9fdd5793c9
kadjiah <kadijahwilson@pursuit.org>
2020-06-21 19:24:25 -0500

    created and tested comment query and route

--- tvwatchlist/server/queries/comments.js
+++ tvwatchlist/server/queries/comments.js
@@ -1,10 +1,20 @@
 const db = require('../db/db')
 
 
-//get all comments
-getAllComments = async () => {
+
+comments = async () => {
     try {
-        let response = db.any("SELECT * from comments")
+        let response = await db.any(`SELECT * from comments`)
+        return response
+    } catch (error) {
+        console.log("error", error)
+    };
+}
+
+//get all comments for specific show_id
+getAllComments = async (show_id) => {
+    try {
+        let response = await db.any(`SELECT * from comments WHERE show_id = $1`, [show_id])
         return response
     } catch (error) {
         console.log("error", error)
@@ -12,10 +22,10 @@ getAllComments = async () => {
 }
 
 //post a comment
-addNewComment = async (user_id, comment_body) => {
+addNewComment = async (user_id, comment_body, show_id) => {
     try {
-    const insertQuery = `INSERT into comments(user_id, comment_body) VALUES($1,$2)`
-    let response = await db.any(insertQuery,[user_id, comment_body])
+    const insertQuery = `INSERT into comments(user_id, comment_body, show_id) VALUES($1,$2, $3)`
+    let response = await db.none(insertQuery,[user_id, comment_body, show_id])
     return response
     } catch (error) {
         console.log("error", error)
@@ -24,5 +34,6 @@ addNewComment = async (user_id, comment_body) => {
 
 module.exports ={
 getAllComments,
-addNewComment
+addNewComment, 
+comments
 }
--- tvwatchlist/server/routes/Comments.js
+++ tvwatchlist/server/routes/Comments.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const commentQueries = require('../queries/comment');
+const commentQueries = require('../queries/comments');
 
 
-//get all comments
 router.get('/', async (req, res) => {
+    try{
+        let response = await commentQueries.comments()
+        res.json({
+            payload: response,
+            message: "all comments retrieved"
+        })
+    }catch(error){
+        res.status(500).json({
+            payload: null,
+            message: 'failed retrieving comments',
+        });
+    }
+})
+
+//get all comments for specific show
+router.get('/show/:show_id', async (req, res) => {
+const { show_id } = req.params.show_id
+// console.log("here", show_id)
     try {
-        let comments = await commentQueries.getAllComments()
+        let comments = await commentQueries.getAllComments(show_id)
         res.json({
             payload: comments,
             message: "all comments retrieved"
@@ -21,19 +38,23 @@ router.get('/', async (req, res) => {
 
 //add comment
 router.post('/', async (req, res) => {
-    const {user_id, comment_body} = req.body
-        try {
-        const newComment = await commentQueries.addNewComment(user_id, comment_body)
+    const {
+        user_id,
+        comment_body,
+        show_id
+    } = req.body
+    try {
+        const newComment = await commentQueries.addNewComment(user_id, comment_body, show_id)
         res.json({
             payload: newComment,
             message: 'added comment'
         })
-        } catch (error) {
-            res.status(500).json({
-                payload: null,
-                message: 'failed',
-            });
-        }
-    })
+    } catch (error) {
+        res.status(500).json({
+            payload: null,
+            message: 'failed',
+        });
+    }
+})
 
-    module.exports = router;
+module.exports = router;
