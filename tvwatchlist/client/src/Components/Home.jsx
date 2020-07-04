36d1dcb27894db0f7e413f80c00522986edfe9e3
kadjiah <kadijahwilson@pursuit.org>
2020-07-04 12:36:50 -0400

    changed id to className added css to the app file instead of creating a css component for home

--- tvwatchlist/client/src/Components/Home.jsx
+++ tvwatchlist/client/src/Components/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
     const Home = () => {
         return (
-          <div>
-            <img id="Homepage" alt="watching tv" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjWjnuxANMboIZk6xcKQLp6MhBpHWiP_loC9T0Np4qAuDrnvwO"/>
-            <h1>Welcome to Tv Watch App </h1>
+          <div className="mainContainer">
+            <h1 className="welcome">Welcome to TV Watch App </h1>
+            <img className="Homepage" alt="watching tv" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjWjnuxANMboIZk6xcKQLp6MhBpHWiP_loC9T0Np4qAuDrnvwO"/>
           </div>
         )
       }
