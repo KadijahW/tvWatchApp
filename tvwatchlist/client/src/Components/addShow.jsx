1ff491d8ab8e74f3ec636f765a5ec42de8912baf
kadjiah <kadijahwilson@pursuit.org>
2020-07-10 22:17:47 -0400

    created form layout, added functionality to add new form

--- tvwatchlist/client/src/Components/addShow.jsx
+++ tvwatchlist/client/src/Components/addShow.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 
 class addShow extends React.Component {
@@ -6,13 +7,113 @@ class addShow extends React.Component {
         super(props)
         this.state = {
             loggedInUser: this.props.match.params.id,
+            genres: [],
+            shows: [],
+            img_url: '',
+            title: '',
+            genre_id: 0
         }
     }
+    componentDidMount() {
+        this.getShows();
+        this.getGenres();
+    }
+
+    getShows = async () => {
+        let allShows = `http://localhost:3010/shows`
+        let res = await axios.get(allShows)
+        // console.log(res.data.payload)
+        this.setState({
+            shows: res.data.payload
+        })
+    }
+
+    getGenres = async () => {
+        let allGenres = `http://localhost:3010/genres`
+        let res = await axios.get(allGenres)
+        // console.log(res.data.payload)
+        this.setState({
+            genres: res.data.payload
+        })
+    }
+
+    addNewShow = async (e) => {
+        e.preventDefault()
+        const { img_url, title, genre_id } = this.state
+            if (img_url !== '' && title !== '' && genre_id !== 0) {
+                let newShow = `http://localhost:3010/shows`
+                 await axios.post(newShow, {img_url, title, genre_id})
+            }
+        this.resetForm()
+    }
+
+    resetForm = () => {
+        this.setState({
+            img_url: '',
+            title: '',
+            genre_id: 0
+        })
+    }
+
+    handleImageURL = async (e) => {
+        this.setState({
+            img_url: e.target.value
+        })
+    }
+
+    handleTitle = async (e) => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+    handleGenre = async (e) => {
+        this.setState({
+            genre_id: e.target.value
+        })
+    }
+
     render() {
+        const { shows, genres } = this.state
         return (
             <div>
-                <form>
-                    
+                <h1>Add Show</h1>
+                <form className='startWatching' onSubmit={this.handleSubmit}>
+                    <h3>Start Watching Show</h3>
+                    <div>
+                        <select onChange={this.handleShow}>
+                            <option>---All Shows---</option>
+                            {shows.map(show => {
+                                return (
+                                    <option value={show.id}>{show.title}</option>
+                                )
+                            })}
+                        </select>
+                        <br></br>
+
+                        <button>Start Watching</button>
+                    </div>
+                </form>
+
+                <h1>Or add new show</h1>
+                <form className='addShow' onSubmit={this.addNewShow}>
+                    <label htmlFor='img_url'>Show Image URL</label>
+                    <br></br>
+                    <input name='img_url' type='text' placeholder='url' onChange={this.handleImageURL}></input>
+                    <br></br>
+                    <label htmlFor='title'>title</label>
+                    <br></br>
+                    <input name='title' type='text' placeholder='name' onChange={this.handleTitle}></input>
+                    <br></br>
+                    <select onChange={this.handleGenre}>
+                        <option>---Select Genre---</option>
+                        {genres.map(genre => {
+                            return (
+                                <option value={genre.id}>{genre.genre_name}</option>
+                            )
+                        })}
+                    </select>
+                    <br></br>
+                    <button>ADD</button>
                 </form>
             </div>
         )
