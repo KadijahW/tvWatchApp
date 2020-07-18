56c5f0eb0ebd2a14b8537d897554fda2fd7a3df8
kadjiah <kadijahwilson@pursuit.org>
2020-07-18 12:58:38 -0400

    working on getting each user that watches each show

--- tvwatchlist/client/src/Components/Shows.jsx
+++ tvwatchlist/client/src/Components/Shows.jsx
@@ -1,52 +1,68 @@
 import React from 'react'
 import axios from 'axios'
 import './CSS/Shows.css'
+import { Link } from 'react-router-dom'
 
 class Shows extends React.Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             shows: [],
-            users: []
+            watchers: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getShows()
     }
 
     getShows = async () => {
         let allShows = `http://localhost:3010/shows`
         let res = await axios.get(allShows)
-
-        console.log(res.data.payload)
         this.setState({
             shows: res.data.payload
         })
     }
 
-    // getUsersWatchingShow = async () => {
-    //     let users = `htttp://localhost:3010/`
-    // }
+    getUsersWatchingShow = async () => {
+        const {show_id} = this.state
+        let users = `http://localhost:3010/shows/watchers/${show_id}`
+        let res = await axios.get(users)
+        console.log(res.data.payload)
+        this.setState({
+            watchers: res.data.payload
+        })
+    }
 
-    render(){
-        const {shows} = this.state
-        return(
-            <div className="showDiv">
-                {shows.map(show => {
-                    return (
-                        <div className="allShows">
-                            <h2>{show.title}</h2>
-                            <h4>{show.genre_name}</h4>
-                            <img className ="shows_img" key = {show.id} src={show.img_url} alt={show.title}/>
-                        </div>
 
-                    )
-                })}
-                <div>
+    render() {
+        const { shows, watchers } = this.state
 
+        const showsArr = shows.map(show => {
+            return (
+                <div className="allShows">
+                    <Link to={`/shows/${show.id}`}><h2>{show.title}</h2></Link>
+                    <h4>{show.genre_name}</h4>
+                    <img className="shows_img" key={show.id} src={show.img_url} alt={show.title} />
                 </div>
+            )
+        })
+
+        const userWatching =  watchers.map(el => {
+            return(
+                    JSON.parse(JSON.stringify(el.username))
+            )
+        })
+        const displayUsers = userWatching.join(', ')
+
+        return (
+            <div className="showDiv">
+                <ul>
+                  {showsArr}
+                </ul>
+
             </div>
+
         )
     }
 }
