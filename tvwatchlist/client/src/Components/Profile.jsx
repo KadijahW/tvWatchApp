ed87e3a4bedfa269ed61e9ff5efd99ec1aff41bb
kadjiah <kadijahwilson@pursuit.org>
2020-07-18 12:57:21 -0400

    saving changes

--- tvwatchlist/client/src/Components/Profile.jsx
+++ tvwatchlist/client/src/Components/Profile.jsx
@@ -7,8 +7,7 @@ class Profile extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            loggedInUser: this.props.match.params.id,
-            userId: "",
+            user_id: this.props.match.params.user_id,
             shows: [],
         }
     }
@@ -17,8 +16,8 @@ class Profile extends React.Component {
     }
 
     getUserShows = async () => {
-        const userId = this.props.match.params.id
-        let userURL = `http://localhost:3010/shows/user/${userId}`
+        const {user_id} = this.state
+        let userURL = `http://localhost:3010/shows/user/${user_id}`
         let res = await axios.get(userURL)
         // console.log(res.data.payload)
         this.setState({
@@ -41,7 +40,7 @@ class Profile extends React.Component {
                         {shows.map(show => {
                             return (
                                     <div className="show">
-                                        <Link className="showTitle" to={"/shows"}><h3>{show.title}</h3></Link>
+                                        <Link className="showTitle" to={`/shows/${show.id}`}><h3>{show.title}</h3></Link>
                                         <h4 className="showGenre">{show.genre_name}</h4>
                                         <img className="showCover" key={show.id} src={show.img_url} alt={show.title} />
                                     </div>
